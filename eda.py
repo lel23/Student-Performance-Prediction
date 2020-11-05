623b22e8c311fc237ae9840eb545e15419c98401
Sam Bluestone <sambluestone@outlook.com>
2020-11-04 23:44:01 -0500

    updated data clean up and random forest feature selection implementation

--- eda.py
+++ eda.py
@@ -10,6 +10,10 @@ from mlxtend.plotting import scatterplotmatrix
 import numpy as np
 import seaborn as sns
 from mlxtend.plotting import heatmap
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectFromModel
+import sys
 
 
 
@@ -21,18 +25,70 @@ df['sex'] = df['sex'].replace(['M', 'F'], [1, 0])
 df['address'] = df['address'].replace(['U', 'R'], [1, 0])
 df['famsize'] = df['famsize'].replace(['GT3', 'LE3'], [1, 0])
 df['Pstatus'] = df['Pstatus'].replace(['T', 'A'], [1, 0])
-df['schoolsup'] = df['schoolsup'].replace(['yes', 'no'], [1, 0])
-df['famsup'] = df['famsup'].replace(['yes', 'no'], [1, 0])
-df['paid'] = df['paid'].replace(['yes', 'no'], [1, 0])
-df['activities'] = df['activities'].replace(['yes', 'no'], [1, 0])
-df['nursery'] = df['nursery'].replace(['yes', 'no'], [1, 0])
-df['higher'] = df['higher'].replace(['yes', 'no'], [1, 0])
-df['internet'] = df['internet'].replace(['yes', 'no'], [1, 0])
-df['romantic'] = df['romantic'].replace(['yes', 'no'], [1, 0])
+df = df.replace(to_replace={['yes', 'GP']:1, ['no', 'MS']:0})
 
 df = pd.get_dummies(df, prefix= ['Mjob', 'Fjob', 'reason', 'guardian'])
 
+#code from: https://stackoverflow.com/questions/46168450/replace-a-specific-range-of-values-in-a-pandas-dataframe
+#convert the scores to integers representing the letter grade range specified in the paper. higher the number, the higher the grade
 df['scores'] = df[['G1', 'G2', 'G3']].mean(axis=1)
+df['scores'] = np.where(df['scores'].between(0, 10), 0, df['scores'])
+df['scores'] = np.where(df['scores'].between(10, 12), 1, df['scores'])
+df['scores'] = np.where(df['scores'].between(12, 14), 2, df['scores'])
+df['scores'] = np.where(df['scores'].between(14, 16), 3, df['scores'])
+df['scores'] = np.where(df['scores'].between(16, 21), 4, df['scores'])
+df['scores'] = df['scores'].astype(np.int)
 
-df.info()
-print(df.head())
+df = df.drop(index=1, columns=['G1', 'G2', 'G3'])
+
+#separate into features and target
+X = df[[i for i in list(df.columns) if i != 'scores']]
+y = df['scores']
+feat_labels = X.columns
+
+#random forest feature selection
+
+stdsc = StandardScaler()
+X = stdsc.fit_transform(X)
+
+forest = RandomForestClassifier(n_estimators=500,
+                                random_state=1)
+
+forest.fit(X, y)
+importances = forest.feature_importances_
+
+indices = np.argsort(importances)[::-1]
+
+for f in range(X.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30, 
+                            feat_labels[indices[f]], 
+                            importances[indices[f]]))
+
+plt.title('Feature Importance')
+plt.bar(range(X.shape[1]), 
+        importances[indices],
+        align='center')
+
+plt.xticks(range(X.shape[1]), 
+           feat_labels[indices], rotation=90)
+plt.xlim([-1, X.shape[1]])
+plt.tight_layout()
+plt.savefig("rf_selection.png")
+plt.show()
+
+sfm = SelectFromModel(forest, prefit=True)
+X_selected = sfm.transform(X)
+print('Number of features that meet this threshold criterion:', 
+      X_selected.shape[1])
+print("Threshold %f" % np.mean(importances))
+
+
+# Now, let's print the  features that met the threshold criterion for feature selection that we set earlier (note that this code snippet does not appear in the actual book but was added to this notebook later for illustrative purposes):
+
+for f in range(X_selected.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30, 
+                            feat_labels[indices[f]], 
+                            importances[indices[f]]))
+
+#df.info()
+#print(df.head())
