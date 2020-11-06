3109971af147992cd8ebcccb8ce41b4be38682f0
JosephSalerno <56265826+JosephSalerno@users.noreply.github.com>
2020-11-05 22:26:41 -0500

    Scatterplot added

--- eda.py
+++ eda.py
@@ -90,6 +90,25 @@ plt.tight_layout()
 plt.show()
 #print('Matrix W:\n', w)
 
+
+X_train_std[0].dot(w)
+
+X_train_pca = X_train_std.dot(w)
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_pca[y_train == l, 0], 
+                X_train_pca[y_train == l, 1], 
+                c=c, label=l, marker=m)
+
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+# plt.savefig('images/05_03.png', dpi=300)
+plt.show()
+
 #############
 
 #random forest feature selection
