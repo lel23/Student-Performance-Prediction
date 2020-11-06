33c4a5fb17b46038e4b94448c3adb82c701f609c
Leslie Le <lel23@mail.wlu.edu>
2020-11-06 14:32:07 -0500

    Removed PCA. Added heatmap

--- eda.py
+++ eda.py
@@ -16,8 +16,6 @@ from sklearn.feature_selection import SelectFromModel
 import sys
 from sklearn.model_selection import train_test_split
 
-
-
 df = pd.read_csv('student-mat-edited.csv')
 
 
@@ -47,72 +45,17 @@ X = df[[i for i in list(df.columns) if i != 'scores']]
 y = df['scores']
 feat_labels = X.columns
 
-#PCA#########
+cols = ['school','sex','age','address','famsize','Pstatus','Medu','Fedu',
+        'at_home','health','other','services','teacher','teacher','other',
+        'services','health','teacher','coruse','other','home','reputation',
+        'mother','father','other','traveltime','studytime','failures',
+        'schoolsup','famsup','paid','activities','nursery','higher','internet',
+        'romantic','famrel','freetime','goout','Dalc','Walc','health',
+        'absences','scores']
 
-X_train, X_test, y_train, y_test =     train_test_split(X, y, test_size=0.3, 
-                     stratify=y,
-                     random_state=0)
-
-
-stdsc = StandardScaler()
-X_train_std = stdsc.fit_transform(X_train)
-X_test_std = stdsc.transform(X_test)
-
-cov_mat = np.cov(X_train_std.T)
-eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
-
-# Make a list of (eigenvalue, eigenvector) tuples
-eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])
-               for i in range(len(eigen_vals))]
-
-# Sort the (eigenvalue, eigenvector) tuples from high to low
-eigen_pairs.sort(key=lambda k: k[0], reverse=True)
-
-
-w = np.hstack((eigen_pairs[0][1][:, np.newaxis],
-               eigen_pairs[1][1][:, np.newaxis]))
-
-
-# plt.savefig('images/05_03.png', dpi=300)
-tot = sum(eigen_vals)
-var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
-cum_var_exp = np.cumsum(var_exp)
-
-plt.bar(range(1, len(df.columns)), var_exp, alpha=0.5, align='center',
-        label='Individual explained variance')
-plt.step(range(1, len(df.columns)), cum_var_exp, where='mid',
-         label='Cumulative explained variance')
-plt.ylabel('Explained variance ratio')
-plt.xlabel('Principal component index')
-plt.legend(loc='best')
-plt.tight_layout()
-# plt.savefig('images/05_02.png', dpi=300)
-plt.show()
-#print('Matrix W:\n', w)
-
-
-X_train_std[0].dot(w)
-
-X_train_pca = X_train_std.dot(w)
-colors = ['r', 'b', 'g', 'y']
-markers = ['s', 'x', 'o', '^']
-
-for l, c, m in zip(np.unique(y_train), colors, markers):
-    plt.scatter(X_train_pca[y_train == l, 0], 
-                X_train_pca[y_train == l, 1], 
-                c=c, label=l, marker=m)
-
-plt.xlabel('PC 1')
-plt.ylabel('PC 2')
-plt.legend(loc='lower left')
-plt.tight_layout()
-# plt.savefig('images/05_03.png', dpi=300)
-plt.show()
-
-#############
-
-#random forest feature selection
-
+"""
+Random Forest Feature Selection
+"""
 stdsc = StandardScaler()
 X = stdsc.fit_transform(X)
 
@@ -157,3 +100,9 @@ for f in range(X_selected.shape[1]):
 
 #df.info()
 #print(df.head())
+
+cm = np.corrcoef(df.values.T)
+hm = heatmap(cm, row_names=cols, column_names=cols)
+plt.tight_layout()
+plt.show()
+
