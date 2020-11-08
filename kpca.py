f4b10ca4310ab64091535ccef3098a84464e1642
Danesh Badlani <38691121+daneshbadlani@users.noreply.github.com>
2020-11-08 15:43:30 -0500

    Update kpca.py

--- kpca.py
+++ kpca.py
@@ -46,6 +46,30 @@ X = df[[i for i in list(df.columns) if i != 'scores']]
 y = df['scores']
 feat_labels = X.columns
 
+
+from sklearn.datasets import make_moons
+X, y = make_moons(n_samples=100, random_state=123)
+plt.scatter(X[y==0, 0], X[y==0, 1], color='red', marker='^', alpha=0.5) 
+plt.scatter(X[y==1, 0], X[y==1, 1], color='blue', marker='o', alpha=0.5)
+plt.tight_layout()
+plt.show()
+
+from sklearn.decomposition import PCA
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
+ax[0].scatter(X_spca[y==0, 0], X_spca[y==0, 1], color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y==1, 0], X_spca[y==1, 1], color='blue', marker='o', alpha=0.5) 
+ax[1].scatter(X_spca[y==0, 0], np.zeros((50,1))+0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y==1, 0], np.zeros((50,1))-0.02, color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+plt.tight_layout()
+plt.show()
+
 from sklearn.decomposition import KernelPCA
 from sklearn.datasets import make_moons
 X, y = make_moons(n_samples=100, random_state=123)
