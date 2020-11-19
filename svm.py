41fed96b69924b77fc5af5f1615fcc7293c1de76
Sam Bluestone <sambluestone@outlook.com>
2020-11-19 14:03:28 -0500

    svm implenetation. 79% accuracy achieved

--- svm.py
+++ svm.py
@@ -12,6 +12,10 @@ from sklearn.utils import resample
 from sklearn.decomposition import PCA, KernelPCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
+from collections import Counter
+
+from imblearn.over_sampling import SMOTE
+
 import sys
 
 df = pd.read_csv('student-mat-edited.csv')
@@ -38,16 +42,25 @@ df['scores'] = df['scores'].astype(np.int)
 
 df = df.drop(index=1, columns=['G1', 'G2', 'G3'])
 
-X = df[[i for i in list(df.columns) if i != "socres"]]
+X = df[[i for i in list(df.columns) if i != "scores"]]
 y = df['scores']
+
+#https://machinelearningmastery.com/multi-class-imbalanced-classification/
+#Get rid of class imbalance in the target feature by oversampling
+oversample = SMOTE()
+X, y = oversample.fit_resample(X, y)
+
+
 
 #list of features in order of importance determined by SBS feature selection
 
-sbs_features = ['J_Dz(e)', 'nHM', 'F01[N-N]', 'F04[C-N]', 'NssssC', 'nCb-', 'C%', 'nCp',
-       'F03[C-N]', 'SdssC', 'HyWi_B(m)', 'LOC', 'SM6_L', 'F03[C-0]', 'Me',
-       'Mi', 'nN-N', 'nArN02', 'nCRX3', 'nCIR', 'B03[C-Cl]', 'SpMax_A',
-       'Psi_i_1d', 'B04[C-Br]', 'NCrt', 'C-026', 'nHDon', 'SpMax_B(m)',
-       'Psi_i_A', 'nN', 'SM6_B(m)']
+sbs_features = ['sex', 'age', 'address', 'famsize', 'Pstatus', 'Medu', 'Fedu',
+       'traveltime', 'studytime', 'failures', 'schoolsup', 'paid',
+       'activities', 'nursery', 'higher', 'internet', 'romantic', 'famrel',
+       'goout', 'Dalc', 'Walc', 'health', 'absences', 'Mjob_at_home',
+       'Mjob_health', 'Mjob_services', 'Mjob_teacher', 'Fjob_at_home',
+       'Fjob_health', 'Fjob_other', 'Fjob_services', 'reason_home',
+       'guardian_father']
 
 
 #list of features in order of importance determined by RF feature selection
@@ -92,12 +105,15 @@ X_test_kpca = scikit_kpca.fit_transform(X_test_std)
 
 #LDA
 
-#normalize the dataset before performing LDA                    
-norm = Normalizer()
-X_train_norm = norm.fit_transform(X_train)
-X_test_norm = norm.transform(X_test)
+#normalize the dataset before performing LDA   
 
-lda = LDA(n_components=2)
+
+norm = Normalizer()
+X_train_norm = norm.fit_transform(X_train[rf_features])
+X_test_norm = norm.transform(X_test[rf_features])
+
+
+lda = LDA(n_components=3)
 X_train_lda = lda.fit_transform(X_train_norm, y_train)
 X_test_lda = lda.transform(X_test_norm)
 
@@ -122,25 +138,26 @@ gs = GridSearchCV(estimator=SVC(class_weight='balanced'),
                   n_jobs=-1)
 
 
-# #grid search for each feature extraction method
-# for X_train_gs, name in zip([X_train_std, X_train_lda, X_train_pca, X_train_kpca], ["No Feature Extraction", "LDA", "PCA", "KPCA"]):
-#     print("\n\nGrid search for", name)
-#     gs = gs.fit(X_train_gs, y_train)
-#     print("Accuracy:", gs.best_score_)
-#     print("Parameters:", gs.best_params_)
+#grid search for each feature extraction method
+for X_train_gs, name in zip([X_train_std, X_train_lda, X_train_pca, X_train_kpca], ["No Feature Extraction", "LDA", "PCA", "KPCA"]):
+    print("\n\nGrid search for", name)
+    gs = gs.fit(X_train_gs, y_train)
+    print("Accuracy:", gs.best_score_)
+    print("Parameters:", gs.best_params_)
 
-# gs = GridSearchCV(estimator=SVC(class_weight='balanced'),
-#                 param_grid=param_grid,
-#                 scoring='accuracy',
-#                 cv=2)
+gs = GridSearchCV(estimator=SVC(class_weight='balanced'),
+                param_grid=param_grid,
+                scoring='accuracy',
+                cv=2)
 
-# scores = cross_val_score(gs, X_train, y_train,
-#                         scoring='accuracy', cv=5)
+scores = cross_val_score(gs, X_train, y_train,
+                        scoring='accuracy', cv=5)
 
-# print("\n\nCV Accuracy: %.3f +/- %.3f" % (np.mean(scores), np.std(scores)))
+print("\n\nCV Accuracy: %.3f +/- %.3f" % (np.mean(scores), np.std(scores)))
 
-#Hyper parameters determined from previous grid search
-svm = SVC(C=.001, kernel='linear', gamma=.1)
+
+Hyper parameters determined from previous grid search
+svm = SVC(C=10, kernel='rbf', gamma=.1)
 
 #Calculate accuracy, precision, recall, and f1-score using each RF and SBS feature selection
 num_features = list(range(2, 44))
@@ -155,22 +172,14 @@ for name, features in zip(["RF", "SBS"], [rf_features, sbs_features]):
         #Code from https://stackoverflow.com/questions/40636514/selecting-pandas-dataframe-column-by-list
         X_train_new = X_train[X_train.columns.intersection(features[:num])]
         X_test_new = X_test[X_test.columns.intersection(features[:num])]
-
-        #Apply standard scaler
-        #X_train_std = stdsc.fit_transform(X_train_new)
-        #X_test_std = stdsc.transform(X_test_new)
-
-        #normalize the dataset before performing LDA                    
-        X_train_norm = norm.fit_transform(X_train_new)
-        X_test_norm = norm.transform(X_test_new)
-
-        X_train_lda = lda.fit_transform(X_train_norm, y_train)
-        X_test_lda = lda.transform(X_test_norm)
+       
+        X_train_std = stdsc.fit_transform(X_train_new)
+        X_test_std = stdsc.transform(X_test_new)
 
         print("\n\nNumber of features:", num)
-            
-        svm.fit(X_train_lda, y_train)
-        y_pred = svm.predict(X_test_lda)
+
+        svm.fit(X_train_std, y_train)
+        y_pred = svm.predict(X_test_std)
 
         accuracy = accuracy_score(y_pred, y_test)
         precision = precision_score(y_pred, y_test, average='weighted')
